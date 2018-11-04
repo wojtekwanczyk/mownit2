7c20f40b60fe050ebe8f5456aeafa59ff28c455c
Wojtek Wanczyk <wojtekwanczyk@gmail.com>
2018-11-04 16:55:22 +0100

    combine two methods into one

--- lab2/jmethod.py
+++ lab2/jmethod.py
@@ -44,11 +44,16 @@ def gen_a(n):
     return a
 
 
-def jacobi_method_1(a, b, n, ro, nr):
+def jacobi_method(a, b, x, n, ro, nr, stop_cond=1):
+    # set start vector
     x1 = np.zeros(n, dtype=my_type)
-    x2 = np.zeros(n, dtype=my_type)
-    xdiff = np.ones(n, dtype=my_type)
-    m = np.zeros((n,n), dtype=my_type)
+
+    for ind, elem in enumerate(x1):
+        x1[ind] = x[ind] + (random.randint(0, 9) * 1e-8)
+
+    x2 = np.full(n, 0, dtype=my_type)
+    xdiff = np.full(n, 100, dtype=my_type)
+    m = np.full((n, n), 0, dtype=my_type)
 
     for i in range(n):
         for j in range(n):
@@ -57,8 +62,13 @@ def jacobi_method_1(a, b, n, ro, nr):
 
     print("Spectral radius: " + str(spectral_radius(m)))
 
+    if stop_cond == 1:
+        cond = np.linalg.norm(xdiff - x1)
+    else:
+        cond = np.linalg.norm(np.dot(a, x1) - b)
+
     k = 0
-    while np.linalg.norm(xdiff - x1) > ro:
+    while cond > ro:
         k += 1
         if k % 1000 == 0:
             print(str(time.process_time()) + ': nr ' + str(nr) + ' iteracja ' + str(k))
@@ -67,39 +77,14 @@ def jacobi_method_1(a, b, n, ro, nr):
             for j in range(n):
                 x2[i] += m[i][j] * x1[j]
 
-        for i in range(n):
-            xdiff[i] = x1[i]
+        xdiff = x1.copy()
+        x1 = x2.copy()
 
-        for i in range(n):
-            x1[i] = x2[i]
+        if stop_cond == 1:
+            cond = np.linalg.norm(xdiff - x1)
+        else:
+            cond = np.linalg.norm(np.dot(a, x1) - b)
 
-    print('Number of iterations: ' + str(k))
-
-    return x1, k
-
-
-def jacobi_method_2(a, b, n, ro, nr):
-    x1 = np.zeros(n, dtype=my_type)
-    x2 = np.zeros(n, dtype=my_type)
-    m = np.zeros((n, n), dtype=my_type)
-
-    for i in range(n):
-        for j in range(n):
-            if i != j:
-                m[i][j] = -(a[i][j] * 1/a[i][i])
-
-    print("Spectral radius: " + str(spectral_radius(m)))
-
-    k = 0
-    while np.linalg.norm(np.dot(a, x1) - b) > ro:
-        k += 1
-        for i in range(n):
-            x2[i] = (1/a[i][i]) * b[i]
-            for j in range(n):
-                x2[i] += m[i][j] * x1[j]
-
-        for i in range(n):
-            x1[i] = x2[i]
     print('Number of iterations: ' + str(k))
 
     return x1, k
@@ -114,7 +99,7 @@ def print_input(a, x, b):
     print(b)
 
 
-def new_jacobi(n, ro, method, result, nr):
+def new_jacobi(n, ro, stop_cond, result, nr):
     a = gen_a(n)
     x = gen_x(n)
     b = np.dot(a, x)
@@ -123,12 +108,7 @@ def new_jacobi(n, ro, method, result, nr):
     print('RO = ' + str(ro))
 
     start_time = time.process_time()
-
-    if method == 1:
-        x_solution, iterations = jacobi_method_1(a, b, n, ro, nr)
-    else:
-        x_solution, iterations = jacobi_method_2(a, b, n, ro, nr)
-
+    x_solution, iterations = jacobi_method(a, b, x, n, ro, nr, stop_cond)
     end_time = time.process_time()
 
     elapsed_time = end_time - start_time
@@ -150,14 +130,14 @@ def main():
     result = manager.list()
     jobs = []
 
-    process = multiprocessing.Process(target=new_jacobi, args=(285, 1e-5, 1, result, 1))
-    jobs.append(process)
-    process = multiprocessing.Process(target=new_jacobi, args=(285, 1e-10, 1, result, 2))
-    jobs.append(process)
-    process = multiprocessing.Process(target=new_jacobi, args=(285, 1e-13, 1, result, 3))
-    jobs.append(process)
+    size = 280
+    stop_cond = 1
 
-    process = multiprocessing.Process(target=new_jacobi, args=(285, 1e-14, 1, result, 4))
+    process = multiprocessing.Process(target=new_jacobi, args=(size, 1e-3, stop_cond, result, 1))
+    jobs.append(process)
+    process = multiprocessing.Process(target=new_jacobi, args=(size, 1e-7, stop_cond, result, 2))
+    jobs.append(process)
+    process = multiprocessing.Process(target=new_jacobi, args=(size, 1e-12, stop_cond, result, 3))
     jobs.append(process)
 
     for j in jobs:
